32b542f69cd2bef0942b019810b988d26d484f95
omar <omardu31270@live.fr>
2023-08-09 18:23:13 +0200

    feat: continue to stylize homepage and sidebar menu

--- homepage/index.js
+++ homepage/index.js
@@ -1,10 +1,17 @@
 const arrowContainer = document.querySelector('.arrow-container');
+const menuMobile = document.querySelector('.menu-icon');
 const sidebar = document.querySelector('.sidebar');
 const overlay = document.querySelector('.overlay');
 const logo = document.querySelector('.logo');
 const close = document.querySelector('.close-button');
 
 arrowContainer.addEventListener('click', () => {
+    sidebar.classList.toggle('open');
+    overlay.classList.toggle('active');
+    logo.classList.toggle('logo-hidden');
+});
+
+menuMobile.addEventListener('click', () => {
     sidebar.classList.toggle('open');
     overlay.classList.toggle('active');
     logo.classList.toggle('logo-hidden');
