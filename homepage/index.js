6b3e9ebe205e6479b5131be660fd5a346303e6b4
omar <omardu31270@live.fr>
2023-08-25 17:23:31 +0200

    feat: add wavy effect on steps section

--- homepage/index.js
+++ homepage/index.js
@@ -29,3 +29,23 @@ close.addEventListener('click', () => {
     overlay.classList.remove('active'); // Désactive l'overlay
     logo.classList.remove('logo-hidden');
 });
+
+// Initialisez ScrollReveal
+const sr = ScrollReveal();
+
+// Configurez les options pour l'effet de balayage latéral
+const revealOptions = {
+    distance: "100%", // Distance de déplacement horizontal (balayage latéral à partir de la droite)
+    duration: 1000, // Durée de l'animation en millisecondes
+    origin: "left", // Direction de laquelle l'élément sera révélé
+    opacity: 0, // Commence avec une opacité de 0 (invisible)
+    reset: true, // Réinitialise l'animation lorsqu'elle est hors de la vue
+    resetOnce: true, // Réinitialise l'animation une fois que l'élément est hors de la vue
+    easing: "ease-in-out", // Type d'animation
+};
+
+// Appliquez l'effet de balayage latéral à chaque élément individuellement
+sr.reveal(".step-one", {...revealOptions, delay: 200 });
+sr.reveal(".step-two", {...revealOptions, delay: 400 });
+sr.reveal(".step-three", {...revealOptions, delay: 600 });
+sr.reveal(".step-four", {...revealOptions, delay: 800 });
