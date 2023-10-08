8247ff7fe7d07b1fc4e59deeae5780c6d2b8be00
omar <omardu31270@live.fr>
2023-10-08 20:09:46 +0200

    fix: correct favicon

--- pages/vendre-mon-vehicule/mail.php
+++ pages/vendre-mon-vehicule/mail.php
@@ -7,10 +7,10 @@
     <meta name="description" content="Nous rachetons votre voiture à Toulouse et ses environs.
     Voiture particulière, collection, accidentée, en panne et utilitaire." />
     <title>Rachat de votre voiture à Toulouse et ses environs</title>
-    <link rel="apple-touch-icon" sizes="180x180" href="../../assets/icons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="../../assets/icons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="../../assets/icons/favicon-16x16.png">
-    <link rel="manifest" href="../../assets/icons/site.webmanifest">
+    <link rel="apple-touch-icon" sizes="180x180" href="../../assets/icons/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../../assets/icons/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../../assets/icons/favicon/favicon-16x16.png">
+    <link rel="manifest" href="../../assets/icons/favicon/site.webmanifest">
     <link rel="stylesheet" href="mail.css" />
 </head>
 
